f5d17da88a2d275473ab0c5f575724fc286b7484
tsmx <strs@freenet.de>
2020-03-02 21:05:39 +0100

    First example method

---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreMicroservice.Controllers
+{
+    [ApiController]
+    [Route("testservice")]
+    public class ExampleController : ControllerBase
+    {
+        private readonly ILogger<ExampleController> _logger;
+
+        public ExampleController(ILogger<ExampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from .Net-Core to " + HttpContext.Connection.RemoteIpAddress + "!";
+        }
+    }
+}
