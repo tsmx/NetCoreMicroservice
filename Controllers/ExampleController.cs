e5e1a295d8fb180b97bbafb9a0ec232fc5e56165
Stricker <Sebastian.Stricker@EXXETA.com>
2020-03-03 15:07:59 +0100

    Post example

--- Controllers/ExampleController.cs
+++ Controllers/ExampleController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace NetCoreMicroservice.Controllers
 {
@@ -18,6 +21,20 @@ namespace NetCoreMicroservice.Controllers
         public string Get()
         {
             return "Hello from .Net-Core to " + HttpContext.Connection.RemoteIpAddress + "!";
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            string requestBody;
+            using (var reader = new StreamReader(Request.Body))
+            {
+                requestBody = reader.ReadToEnd();
+            }
+            var obj = JsonConvert.DeserializeObject(requestBody);
+            Response.StatusCode = 200;
+            Response.Body.Write(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(obj, Formatting.Indented)));
+            Response.StartAsync();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +19,10 @@ namespace NetCoreMicroservice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
             services.AddControllers();
         }
 
