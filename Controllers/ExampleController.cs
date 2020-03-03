d12884380d5b3e86331869cbd665096fb9ad6f23
Stricker <Sebastian.Stricker@EXXETA.com>
2020-03-03 16:27:46 +0100

    Exception handling for JSON deserialization

--- Controllers/ExampleController.cs
+++ Controllers/ExampleController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -27,11 +28,22 @@ namespace NetCoreMicroservice.Controllers
         public void Post()
         {
             string requestBody;
+            object obj = null;
             using (var reader = new StreamReader(Request.Body))
             {
                 requestBody = reader.ReadToEnd();
             }
-            var obj = JsonConvert.DeserializeObject(requestBody);
+            try
+            {
+                obj = JsonConvert.DeserializeObject(requestBody);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                Response.Body.Write(Encoding.ASCII.GetBytes("Invalid JSON request: " + ex.Message));
+                Response.StartAsync();
+                return;
+            }
             Response.StatusCode = 200;
             Response.Body.Write(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(obj, Formatting.Indented)));
             Response.StartAsync();
